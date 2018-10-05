fdc7edbe9bb7abdbfb970cccb85e5d7a6949e736
lxsang <xsang.le@gmail.com>
2018-10-06 00:43:18 +0200

    non blocking version of wterm, for the new api

--- wterm.c
+++ wterm.c
@@ -9,23 +9,201 @@
 #include <string.h>
 #include "plugin.h"
 
+typedef struct{
+	int fdm;
+	void* request;
+	pid_t pid;
+} wterm_rq_t;
+
 void init()
 {
-	
 }
-void pexit()
+void destroy()
 {
-	
 }
-void handle(void* cl, const char* m, const char* rqp, dictionary rq)
+
+void *process(void *data)
 {
-	ws_msg_header_t* h = NULL;
-	if(ws_enable(rq))
+	char buff[BUFFLEN];
+	int max_fdm, status;
+	fd_set fd_in;
+	wterm_rq_t* wterm_data = (wterm_rq_t*)data;
+	antd_request_t *rq = (antd_request_t *)wterm_data->request;
+	ws_msg_header_t *h = NULL;
+	antd_task_t *task = NULL;
+	void *cl = (void *)rq->client;
+	int cl_fd = ((antd_client_t *)cl)->sock;
+	int fdm = wterm_data->fdm;
+	pid_t pid = wterm_data->pid;
+	// first we need to verify if child exits
+	// if it is the case then quit the session
+	// and return an empty task
+	pid_t wpid = waitpid(pid, &status, WNOHANG);
+	if(wpid == -1 || wpid > 0)
 	{
-		
+		// child exits
+		LOG("Child process finished\n");
+		task = antd_create_task(NULL, (void *)rq, NULL);
+		task->priority++;
+		//ws_close(cl, 1011);
+		free(wterm_data);
+		return task;
+	}
+	struct timeval timeout;      
+	timeout.tv_sec = 0;
+	timeout.tv_usec = 500;
+	// otherwise, check if data available
+
+	FD_ZERO(&fd_in);
+	//FD_SET(0, &fd_in);
+	FD_SET(fdm, &fd_in);
+	FD_SET(cl_fd, &fd_in);
+	max_fdm = fdm > cl_fd ? fdm : cl_fd;
+	int rc = select(max_fdm + 1, &fd_in, NULL, NULL, &timeout);
+	switch (rc)
+	{
+	case -1:
+		LOG("Error %d on select()\n", errno);
+		task = antd_create_task(NULL, (void *)rq, NULL);
+		task->priority++;
+		free(wterm_data);
+		ws_close(cl, 1011);
+		return task;
+
+	default:
+	{
+		// If data is on websocket side
+		if (FD_ISSET(cl_fd, &fd_in))
+		{
+			//LOG("data from client\n");
+			h = ws_read_header(cl);
+			if (h)
+			{
+				if (h->mask == 0)
+				{
+					LOG("%s\n", "Data is not mask");
+					write(fdm, "exit\n", 5);
+					free(h);
+					goto wait_for_child;
+				}
+				if (h->opcode == WS_CLOSE)
+				{
+					LOG("%s\n", "Websocket: connection closed");
+					write(fdm, "exit\n", 5);
+					free(h);
+					goto wait_for_child;
+				}
+				else if (h->opcode == WS_TEXT)
+				{
+					int l;
+					char *tok = NULL;
+					char *tok1 = NULL;
+					char *orgs = NULL;
+					while ((l = ws_read_data(cl, h, sizeof(buff), buff)) > 0)
+					{
+						char c = buff[0];
+						switch (c)
+						{
+						case 'i': // input from user
+							write(fdm, buff + 1, l - 1);
+							break;
+
+						case 's': // terminal resize event
+							buff[l] = '\0';
+							tok = strdup(buff + 1);
+							orgs = tok;
+							tok1 = strsep(&tok, ":");
+							if (tok != NULL && tok1 != NULL)
+							{
+								int cols = atoi(tok1);
+								int rows = atoi(tok);
+								//free(tok);
+								struct winsize win = {0, 0, 0, 0};
+								if (ioctl(fdm, TIOCGWINSZ, &win) != 0)
+								{
+									if (errno != EINVAL)
+									{
+										break;
+									}
+									memset(&win, 0, sizeof(win));
+								}
+								//printf("Setting winsize\n");
+								if (rows >= 0)
+									win.ws_row = rows;
+								if (cols >= 0)
+									win.ws_col = cols;
+
+								if (ioctl(fdm, TIOCSWINSZ, (char *)&win) != 0)
+									printf("Cannot set winsize\n");
+
+								free(orgs);
+							}
+
+							break;
+
+						default:
+							break;
+						}
+						//ws_t(cl,buff);
+					}
+					/*if(l == -1)
+									{
+										printf("EXIT FROM CLIENT \n");
+				   						write(fdm, "exit\n", 5);
+				      					return;
+									}*/
+				}
+
+				free(h);
+			}
+			else
+			{
+				write(fdm, "exit\n", 5);
+				ws_close(cl, 1000);
+			}
+		}
+		// If data on master side of PTY
+		if (FD_ISSET(fdm, &fd_in))
+		{
+			//LOG("data from server\n");
+			//rc = read(fdm, buff, sizeof(buff));
+			if ((rc = read(fdm, buff, sizeof(buff) - 1)) > 0)
+			{
+				// Send data to websocket
+				buff[rc] = '\0';
+				ws_t(cl, buff);
+			}
+			else
+			{
+				if (rc < 0)
+				{
+					LOG("Error %d on read standard input. Exit now\n", errno);
+					write(fdm, "exit\n", 5);
+					ws_close(cl, 1011);
+					goto wait_for_child;
+				}
+			}
+		}
+		//printf("DONE\n");
+	}
+	} // End switch
+	wait_for_child:
+	task = antd_create_task(process, (void *)wterm_data, NULL);
+	task->priority++;
+	task->type = HEAVY;
+	return task;
+}
+
+void *handle(void *rqdata)
+{
+	antd_request_t *rq = (antd_request_t *)rqdata;
+	ws_msg_header_t *h = NULL;
+	antd_task_t *task = antd_create_task(NULL, (void *)rq, NULL);
+	task->priority++;
+	void *cl = (void *)rq->client;
+	if (ws_enable(rq->request))
+	{
 		int fdm, fds;
-		int rc, status;
-		char buff[1024];
 		pid_t pid;
 		// Check arguments
 		fdm = posix_openpt(O_RDWR);
@@ -33,23 +211,23 @@ void handle(void* cl, const char* m, const char* rqp, dictionary rq)
 		{
 			LOG("Error %d on posix_openpt()\n", errno);
 			ws_close(cl, 1011);
-			return ;
+			return task;
 		}
 
-		rc = grantpt(fdm);
+		int rc = grantpt(fdm);
 		if (rc != 0)
 		{
 			LOG("Error %d on grantpt()\n", errno);
 			ws_close(cl, 1011);
-			return ;
+			return task;
 		}
 
 		rc = unlockpt(fdm);
 		if (rc != 0)
 		{
-			LOG( "Error %d on unlockpt()\n", errno);
+			LOG("Error %d on unlockpt()\n", errno);
 			ws_close(cl, 1011);
-			return ;
+			return task;
 		}
 
 		// Open the slave side ot the PTY
@@ -59,153 +237,23 @@ void handle(void* cl, const char* m, const char* rqp, dictionary rq)
 		pid = fork();
 		if (pid)
 		{
-			fd_set fd_in;
-
-			// FATHER
-
-			// Close the slave side of the PTY
-			close(fds);
-			int max_fdm;
-			int cl_fd = ((antd_client_t*)cl)->sock;
-			while (1)
-			{	
-				FD_ZERO(&fd_in);
-				//FD_SET(0, &fd_in);
-				FD_SET(fdm, &fd_in);
-				FD_SET(cl_fd,&fd_in);
-				max_fdm = fdm>cl_fd?fdm:cl_fd;
-				rc = select(max_fdm + 1, &fd_in, NULL, NULL, NULL);
-				switch(rc)
-				{
-					case -1 : 
-						LOG("Error %d on select()\n", errno);
-						ws_close(cl, 1011);
-						goto wait_for_child;
-
-					default :
-					{
-	   					// If data is on websocket side
-						if (FD_ISSET(cl_fd, &fd_in))
-						{
-			      			h = ws_read_header(cl);
-			      			if(h)
-			      			{
-								if(h->mask == 0)
-								{
-			      					LOG("%s\n","Data is not mask");
-			   						write(fdm, "exit\n", 5);
-									free(h);
-			      					goto wait_for_child;;
-								}
-			      				if(h->opcode == WS_CLOSE)
-			      				{
-			      					LOG("%s\n","Websocket: connection closed");
-			   						write(fdm, "exit\n", 5);
-									free(h);
-			      					goto wait_for_child;;
-			      				}
-			      				else if(h->opcode == WS_TEXT)
-			      				{
-			      					int l;
-									char * tok = NULL;
-									char* tok1 = NULL;
-									char* orgs = NULL;
-			      					while((l = ws_read_data(cl,h,sizeof(buff),buff)) > 0)
-			      					{
-										char c = buff[0];
-										switch(c)
-										{
-											case 'i': // input from user
-											write(fdm, buff+1, l-1);
-											break;
-											
-											case 's': // terminal resize event
-											buff[l] = '\0';
-											tok = strdup(buff+1);
-											orgs = tok;
-											tok1 = strsep(&tok,":");
-											if(tok != NULL && tok1 != NULL)
-											{
-												int cols = atoi(tok1);
-												int rows = atoi(tok);
-												//free(tok);
-												struct winsize win = { 0, 0, 0, 0 };
-												if (ioctl(fdm, TIOCGWINSZ, &win) != 0) {
-													if (errno != EINVAL) {
-															printf("Exit now \n");
-															break;
-													}
-													memset(&win, 0, sizeof(win));
-												}
-												//printf("Setting winsize\n");
-												if (rows >= 0)
-													win.ws_row = rows;
-												if (cols >= 0)
-													win.ws_col = cols;
-
-												if (ioctl(fdm, TIOCSWINSZ, (char *) &win) != 0)
-													printf("Cannot set winsize\n");
-												
-												free(orgs);
-											}
-											
-											break;
-											
-											default:
-											break;
-										}
-										//ws_t(cl,buff);
-			      					}
-									/*if(l == -1)
-									{
-										printf("EXIT FROM CLIENT \n");
-				   						write(fdm, "exit\n", 5);
-				      					return;
-									}*/
-			      				}
-
-			      				free(h);
-			      			} 
-							else
-							{
-								write(fdm, "exit\n", 5);
-								ws_close(cl,1000);
-							} 
-						}
-						// If data on master side of PTY
-						if (FD_ISSET(fdm, &fd_in))
-						{
-							//rc = read(fdm, buff, sizeof(buff));
-							if ( (rc = read(fdm, buff,sizeof(buff)-1)) > 0)
-							{
-								// Send data to websocket
-								buff[rc] = '\0';
-								ws_t(cl,buff);
-							} else
-							{
-								if (rc < 0)
-								{
-									LOG("Error %d on read standard input. Exit now\n", errno);
-									write(fdm, "exit\n", 5);
-									ws_close(cl,1011);
-									goto wait_for_child;
-								}
-							}
-						}
-						//printf("DONE\n");
-					}
-				} // End switch
-			} // End while
-			wait_for_child:
-				waitpid(pid, &status, 0);
+			free(task);
+			wterm_rq_t* wdata = (wterm_rq_t*)malloc(sizeof(*wdata));
+			wdata->request = rqdata;
+			wdata->fdm = fdm;
+			wdata->pid = pid;
+			task = antd_create_task(process, (void*)wdata ,NULL);
+			task->priority++;
+			task->type = HEAVY;
+			return task;
 		}
 		else
 		{
-			struct termios slave_orig_term_settings; // Saved terminal settings
-			struct termios new_term_settings; // Current terminal settings
+			//struct termios slave_orig_term_settings; // Saved terminal settings
+			//struct termios new_term_settings; // Current terminal settings
 
 			// CHILD
-		
+
 			// Close the master side of the PTY
 			close(fdm);
 
@@ -236,37 +284,14 @@ void handle(void* cl, const char* m, const char* rqp, dictionary rq)
 			// As the child is a session leader, set the controlling terminal to be the slave side of the PTY
 			// (Mandatory for programs like the shell to make them manage correctly their outputs)
 			ioctl(0, TIOCSCTTY, 1);
-		
+
 			//system("/bin/bash");
 			system("TERM=linux sudo login");
 			// if Error...
-			ws_close(cl,1000);
+			ws_close(cl, 1000);
 			//LOG("%s\n","Terminal exit");
 			_exit(1);
 		}
 	}
-
-	LOG("%s\n","All processes exit");
+	return task;
 }
-
-/*
-static void set_window_size(int rows, int cols)
-{
-struct winsize win = { 0, 0, 0, 0 };
-if (ioctl(STDIN_FILENO, TIOCGWINSZ, &win)) {
-	if (errno != EINVAL) {
-			goto bail;
-		}
-		memset(&win, 0, sizeof(win));
-	}
-
-	if (rows >= 0)
-		win.ws_row = rows;
-	if (cols >= 0)
-		win.ws_col = cols;
-
-	if (ioctl(STDIN_FILENO, TIOCSWINSZ, (char *) &win))
-bail:
-		perror_on_device("%s");
-}
-*/
